87cc4b18b3d58df8b225c5e09f5b598e967654bf
stampe123 <jdomeij@gmail.com>
2017-02-28 21:33:12 +0100

    Add unit tests for lifx-light, fix code style

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Lifx nodes for Node-Red
-An solution to control Lifx lights using Node-Red, uses [node-lifx library](https://github.com/MariusRumpf/node-lifx) for communicating with the lights.
+An solution to control Lifx lights over local network using Node-Red, uses [node-lifx library](https://github.com/MariusRumpf/node-lifx) for communicating with the lights.
 
 This module provides input and output nodes for communicating with Lifx lights, the input node accepts multiple color format and automatically converts the values to the right format. 
 
---
+++ test/lifx-light.test.js
@@ -0,0 +1,591 @@
+/* eslint-env mocha */
+"use strict";
+
+var LifxLight = require('../lib/lifx-light.js');
+//var _ = require('lodash');
+
+var chai      = require('chai');
+var chaiSpies = require('chai-spies');
+
+chai.use(chaiSpies);
+
+var expect = chai.expect;
+
+
+const defaultID = '123456789';
+/**
+ * Emulates node-lifx
+ */
+function LifxEmulator() {
+  var self = this;
+
+  self.id = '123456789';
+  self.address = '1.1.1.1';
+  self.port = 56700;
+  self.label = null;
+  self.status = 'on';
+  self.seenOnDiscovery = 1;
+
+  // getState return values
+  this._getStateErr = null;
+  this._getStateData = {
+    color: {
+      hue: 0,
+      saturation: 100,
+      brightness: 100,
+      kelvin: 4000
+    },
+    power: 1,
+    label: 'Test Lifx'
+  };
+
+  this.getState = function(callback) {
+    callback(self._getStateErr, self._getStateData);
+  };
+
+  // getHardwareVersion return values
+  this._getHWVerErr = null;
+  this._getHWVerData = {
+    vendorId: 1,
+    productId: 1,
+    version: 6,
+    vendorName: 'LIFX',
+    productName: 'Original 1000',
+    productFeatures: {
+      color: true,
+      infrared: false,
+      multizone: false
+    }
+  };
+
+  this.getHardwareVersion = function(callback) {
+    callback(self._getHWVerErr, self._getHWVerData);
+  };
+
+}
+
+
+describe('Lifx-Light', () => {
+  describe('constructor', () => {
+    it('Default light', (done) => {
+      var lightItem = new LifxLight(defaultID, new LifxEmulator, {});
+      expect(lightItem.id).to.equal(defaultID);
+      done();
+    });
+  });
+
+  describe('parseColorRGB', () => {
+    var lightItem, lifxItem;
+    beforeEach((done) => {
+      lifxItem = new LifxEmulator();
+      lifxItem._getStateData.color = {
+        hue: 0,
+        saturation: 100,
+        brightness: 100,
+        kelvin: 3500
+      };
+
+      lightItem = new LifxLight(lifxItem.id, lifxItem, {});
+      lightItem.initialize((err) => {
+        if (err)
+          return done(err);
+        lightItem.stop();
+        done();
+      });
+    });
+    afterEach(() => {
+      lightItem = null;
+      lifxItem = null;
+    });
+
+
+    it('hue: 123', (done) => {
+      var output = {};
+      var tmp;
+      tmp = lightItem.parseColorRGB({'hue': 123}, output);
+
+      expect(tmp).to.equal(true);
+      expect(output).to.be.an('object');
+      expect(output.hue).to.equal(123);
+
+      done();
+    });
+
+    it('hex: #ff0000', (done) => {
+      var output = {};
+      var tmp;
+      tmp = lightItem.parseColorRGB({'hex': '#ff0000'}, output);
+
+      expect(tmp).to.equal(true);
+      expect(output).to.be.an('object');
+      expect(output.hue).to.equal(0);
+      expect(output.saturation).to.equal(100);
+      expect(output.brightness).to.equal(100);
+
+      done();
+    });
+
+
+    it('hex: #0077FF', (done) => {
+      var output = {};
+      var tmp;
+      tmp = lightItem.parseColorRGB({'hex': '#123456'}, output);
+
+      expect(tmp).to.equal(true);
+      expect(output).to.be.an('object');
+      expect(output.hue).to.equal(210);
+      expect(output.saturation).to.equal(79);
+      expect(output.brightness).to.equal(34);
+
+      done();
+    });
+
+    it('red: 0x12, green: 0x34, blue: 0x56', (done) => {
+      var output = {};
+      var tmp;
+      tmp = lightItem.parseColorRGB({red: 0x12, green: 0x34, blue: 0x56}, output);
+
+      expect(tmp).to.equal(true);
+      expect(output).to.be.an('object');
+      expect(output.hue).to.equal(210);
+      expect(output.saturation).to.equal(79);
+      expect(output.brightness).to.equal(34);
+
+      done();
+    });
+
+    it('green: 0xFFF, red: NaN', (done) => {
+      var output = {};
+      var tmp;
+      tmp = lightItem.parseColorRGB({green: 0xFFF}, output);
+
+      expect(tmp).to.equal(true);
+      expect(output).to.be.an('object');
+      expect(output.hue).to.equal(60);
+      expect(output.saturation).to.equal(100);
+      expect(output.brightness).to.equal(100);
+
+      done();
+    });
+
+    it('blue: -1', (done) => {
+      var output = {};
+      var tmp;
+      tmp = lightItem.parseColorRGB({blue: -1}, output);
+
+      expect(tmp).to.equal(true);
+      expect(output).to.be.an('object');
+      expect(output.hue).to.equal(0);
+      expect(output.saturation).to.equal(100);
+      expect(output.brightness).to.equal(100);
+
+      done();
+    });
+
+
+    it('rgb: [0x12, 0x34, 0x56]', (done) => {
+      var output = {};
+      var tmp;
+      tmp = lightItem.parseColorRGB({rgb: [0x12, 0x34, 0x56]}, output);
+
+      expect(tmp).to.equal(true);
+      expect(output).to.be.an('object');
+      expect(output.hue).to.equal(210);
+      expect(output.saturation).to.equal(79);
+      expect(output.brightness).to.equal(34);
+
+      done();
+    });
+
+
+    it('hue: 123', (done) => {
+      var output = {};
+      var tmp;
+      tmp = lightItem.parseColorRGB({'hex': '#123456'}, output);
+
+      expect(tmp).to.equal(true);
+      expect(output).to.be.an('object');
+      expect(output.hue).to.equal(210);
+      expect(output.saturation).to.equal(79);
+      expect(output.brightness).to.equal(34);
+
+      done();
+    });
+
+    it('sat: 33', (done) => {
+      var output = {};
+      var tmp;
+      tmp = lightItem.parseColorRGB({'sat': 33}, output);
+
+      expect(tmp).to.equal(true);
+      expect(output).to.be.an('object');
+      expect(output.hue).to.equal(0);
+      expect(output.saturation).to.equal(33);
+      expect(output.brightness).to.equal(100);
+
+      done();
+    });
+
+    it('saturation: 33', (done) => {
+      var output = {};
+      var tmp;
+      tmp = lightItem.parseColorRGB({'saturation': 33}, output);
+
+      expect(tmp).to.equal(true);
+      expect(output).to.be.an('object');
+      expect(output.hue).to.equal(0);
+      expect(output.saturation).to.equal(33);
+      expect(output.brightness).to.equal(100);
+
+      done();
+    });
+  });
+
+  describe('parseColorTemp', () => {
+    var lightItem, lifxItem;
+    beforeEach((done) => {
+      lifxItem = new LifxEmulator();
+      lifxItem._getStateData.color = {
+        hue: 0,
+        saturation: 100,
+        brightness: 100,
+        kelvin: 3500
+      };
+
+      lightItem = new LifxLight(lifxItem.id, lifxItem, {});
+      lightItem.initialize((err) => {
+        if (err)
+          return done(err);
+        lightItem.stop();
+        done();
+      });
+    });
+    afterEach(() => {
+      lightItem = null;
+      lifxItem = null;
+    });
+
+    it('kelvin: 3000', (done) => {
+      var output = {};
+      var tmp;
+      tmp = lightItem.parseColorTemp({'kelvin': 3000}, output);
+
+      expect(tmp).to.equal(true);
+      expect(output).to.be.an('object');
+      expect(output.kelvin).to.equal(3000);
+
+      done();
+    });
+
+    it('ct: 1000000/3000', (done) => {
+      var output = {};
+      var tmp;
+      tmp = lightItem.parseColorTemp({'ct': 1000000/3000}, output);
+
+      expect(tmp).to.equal(true);
+      expect(output).to.be.an('object');
+      expect(output.kelvin).to.equal(3000);
+
+      done();
+    });
+
+    it('mired: 1000000/3000', (done) => {
+      var output = {};
+      var tmp;
+      tmp = lightItem.parseColorTemp({'mired': 1000000/3000}, output);
+
+      expect(tmp).to.equal(true);
+      expect(output).to.be.an('object');
+      expect(output.kelvin).to.equal(3000);
+
+      done();
+    });
+
+    it('mirek: 1000000/3000', (done) => {
+      var output = {};
+      var tmp;
+      tmp = lightItem.parseColorTemp({'mirek': 1000000/3000}, output);
+
+      expect(tmp).to.equal(true);
+      expect(output).to.be.an('object');
+      expect(output.kelvin).to.equal(3000);
+
+      done();
+    });
+
+
+  });
+
+  describe('parseBrightness', () => {
+    var lightItem, lifxItem;
+    beforeEach((done) => {
+      lifxItem = new LifxEmulator();
+      lifxItem._getStateData.color = {
+        hue: 0,
+        saturation: 0,
+        brightness: 1,
+        kelvin: 3500
+      };
+
+      lightItem = new LifxLight(defaultID, lifxItem, {});
+      lightItem.initialize((err) => {
+        if (err)
+          return done(err);
+        lightItem.stop();
+        done();
+      });
+    });
+    afterEach(() => {
+      lightItem = null;
+      lifxItem = null;
+    });
+
+    it('bri: 33', (done) => {
+      var output = {};
+      var tmp;
+      tmp = lightItem.parseBrightness({'bri': 33}, output);
+
+      expect(tmp).to.equal(true);
+      expect(output).to.be.an('object');
+      expect(output.brightness).to.equal(33);
+
+      done();
+    });
+
+    it('brightness: 33', (done) => {
+      var output = {};
+      var tmp;
+      tmp = lightItem.parseBrightness({'brightness': 33}, output);
+
+      expect(tmp).to.equal(true);
+      expect(output).to.be.an('object');
+      expect(output.brightness).to.equal(33);
+
+      done();
+    });
+  });
+
+  describe('setColor', () => {
+    var lightItem, lifxItem;
+    beforeEach((done) => {
+      lifxItem = new LifxEmulator();
+      lifxItem.color = chai.spy();
+      lifxItem.on    = chai.spy();
+      lifxItem.off   = chai.spy();
+
+      lightItem = new LifxLight(defaultID, lifxItem, true);
+      lightItem.initialize((err) => {
+        if (err)
+          return done(err);
+        lightItem.stop();
+        done();
+      });
+    });
+    afterEach(() => {
+      lightItem = null;
+      lifxItem = null;
+    });
+
+    it('hex: #0000FF', (done) => {
+      lightItem.setColor({hex: '#0000FF'});
+      //console.log(lifxItem.color.__spy.calls[0]);
+      expect(lifxItem.color).to.have.been.called();
+      expect(lifxItem.color).to.have.been.called.with.exactly(240, 100, 100, 4000, 0);
+      done();
+    });
+  
+    it('kelvin: 3500', (done) => {
+      lightItem.setColor({kelvin: 3500});
+      expect(lifxItem.color).to.have.been.called();
+      expect(lifxItem.color).to.have.been.called.with.exactly(0, 100, 100, 3500, 0);
+      done();
+    });
+
+    it('brightness: 50', (done) => {
+      lightItem.setColor({brightness: 50});
+      expect(lifxItem.color).to.have.been.called();
+      expect(lifxItem.color).to.have.been.called.with.exactly(0, 100, 50, 4000, 0);
+      done();
+    });
+
+    it('{ on: false, hex: "#00ff00"}', (done) => {
+      lightItem.setColor({ on: false, hex: '#00ff00', duration: 1000 });
+      expect(lifxItem.off).to.have.been.called();
+      expect(lifxItem.color).to.have.not.been.called();
+      done();
+    });
+
+    it('false (off)', (done) => {
+      lightItem.setColor(false);
+      expect(lifxItem.off).to.have.been.called();
+      done();
+    });
+
+    it('"off"', (done) => {
+      lightItem.setColor("off");
+      expect(lifxItem.off).to.have.been.called();
+      done();
+    });
+
+    it('50 (brightness)', (done) => {
+      lightItem.setColor(50);
+      expect(lifxItem.color).to.have.been.called();
+      expect(lifxItem.color).to.have.been.called.with.exactly(0, 100, 50, 4000, 0);
+      done();
+    });
+
+    it('50% bri over 5s', (done) => {
+      lightItem.setColor({ 'bri': 50, duration: 5000});
+      expect(lifxItem.color).to.have.been.called();
+      expect(lifxItem.color).to.have.been.called.with.exactly(0, 100, 50, 4000, 5000);
+      done();
+    });
+
+    it('Off->On (50%)', (done) => {
+      lightItem.state.on = false;
+      lightItem.setColor({'on': true, bri: 50, duration: 5000});
+      expect(lifxItem.on).to.have.been.called();
+      expect(lifxItem.color).to.have.been.called();
+      expect(lifxItem.on).to.have.been.called.with.exactly(5000);
+      expect(lifxItem.color).to.have.been.called.with.exactly(0, 100, 50, 4000, 0);
+      done();
+    });
+  });
+
+
+  describe('getStateMessage', () => {
+    var lightItem, lifxItem;
+    beforeEach((done) => {
+      lifxItem = new LifxEmulator();
+      lightItem = new LifxLight(defaultID, lifxItem, true);
+      lightItem.initialize((err) => {
+        if (err)
+          return done(err);
+        lightItem.stop();
+        done();
+      });
+    });
+    afterEach(() => {
+      lightItem = null;
+      lifxItem = null;
+    });
+
+    it('getStateMessage', (done) => {
+      var tmp = lightItem.getStateMessage();
+      expect(tmp.id).to.equal(defaultID);
+      done();
+    });
+  });
+
+
+  describe('pollChanges', () => {
+    var lightItem, lifxItem;
+    beforeEach((done) => {
+      lifxItem = new LifxEmulator();
+      lightItem = new LifxLight(defaultID, lifxItem, true);
+      lightItem.initialize((err) => {
+        if (err)
+          return done(err);
+        lightItem.stop();
+        done();
+      });
+    });
+    afterEach(() => {
+      lightItem = null;
+      lifxItem = null;
+    });
+
+    it('Basic test', (done) => {
+      lightItem.pollChanges();
+      done();
+    });
+  });
+
+  describe('setReachable', () => {
+    var lightItem, lifxItem;
+    beforeEach((done) => {
+      lifxItem = new LifxEmulator();
+      lightItem = new LifxLight(defaultID, lifxItem, true);
+      lightItem.initialize((err) => {
+        if (err)
+          return done(err);
+        lightItem.stop();
+        done();
+      });
+    });
+    afterEach(() => {
+      lightItem.stop();
+      lightItem = null;
+      lifxItem = null;
+    });
+
+    it('false', (done) => {
+      lightItem.setReachable(false);
+
+      done();
+    });
+
+    it('false', (done) => {
+      lightItem.setReachable(false);
+      
+      lightItem.pollChanges = chai.spy();
+
+      lightItem.setReachable(true);
+      expect(lightItem.pollChanges).to.have.been.called();
+      done();
+    });
+
+  });
+
+  describe('IR Support', () => {
+    var lightItem, lifxItem;
+    beforeEach((done) => {
+      lifxItem = new LifxEmulator();
+      lifxItem._getHWVerData.productFeatures.infrared = true;
+
+      lifxItem.getMaxIR = function(callback) {
+        callback(null, 33);
+      };
+
+      lightItem = new LifxLight(lifxItem.id, lifxItem, {});
+      lightItem.initialize((err) => {
+        if (err)
+          return done(err);
+        lightItem.stop();
+        done();
+      });
+    });
+    afterEach(() => {
+      lightItem = null;
+      lifxItem = null;
+    });
+
+    it('getStateMessage', (done) => {
+      var ret = lightItem.getStateMessage();
+      expect(ret.maxIR).to.equal(33);
+      done();
+    });
+
+    it('setColor', (done) => {
+      lifxItem.color = chai.spy();
+      lifxItem.on    = chai.spy();
+      lifxItem.off   = chai.spy();
+      lifxItem.maxIR = chai.spy();
+
+      lightItem.setColor({maxIR: 66});
+
+      expect(lifxItem.on).not.to.have.been.called();
+      expect(lifxItem.color).not.to.have.been.called();
+
+      expect(lifxItem.maxIR).to.have.been.called();
+      expect(lifxItem.maxIR).to.have.been.called.with.exactly(66);
+      done();
+    });
+
+    it('pollChanges', (done) => {
+      lightItem.pollChanges();
+
+      done();
+    });
+  });
+});
